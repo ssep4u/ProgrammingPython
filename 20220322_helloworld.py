13823dc0ddaea15651fc224deef7d69888c13bef
SSep <ssep4u@gmail.com>
2022-03-22 10:14:04 +0900

    변수 선언, 초기화, 여러 가지 출력하는 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,3 +1,7 @@
 print("Hello, World!")  #큰따, 작따 같음   //
-print('Hello, 임정훈!')
+name='양다연'
+print('Hello, '+name+'!')   #java 형식
+print('Hello, %s!' % name)  #c 형식
+print('Hello, {}!'.format(name))    #format함수
+print(f'Hello, {name}!')    #f-string 형식
 print("Red velvet's favorite song is Red flavour")
