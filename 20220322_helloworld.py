c68517e37a38ef967e736da182f23765d7dd6eb2
SSep <ssep4u@gmail.com>
2022-03-29 10:18:15 +0900

    변수, int, float, str, \n, type(), //:몫, %

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -5,3 +5,7 @@ print('Hello, %s!' % name)  #c 형식
 print('Hello, {}!'.format(name))    #format함수
 print(f'Hello, {name}!')    #f-string 형식
 print("Red velvet's favorite song is Red flavour")
+
+student_number = 3419
+name = '임정훈'
+print(f'학번: {student_number}, 이름: {name}')     #학번: 3419, 이름: 임정훈
---
+++ 20220329_type.py
@@ -0,0 +1,25 @@
+#숫자
+student_number = 3419
+age = 18
+height = 176.6
+
+#문자
+name = '임정훈'
+
+print(f'학번: {student_number}\n이름: {name}\n나이: {age}\n키: {height}')  #학번: 3419
+
+print(type(student_number)) #<class 'int'>
+print(type(name)) #<class 'str'>
+print(type(age)) #<class 'int'>
+print(type(height)) #<class 'float'>
+print(type(10.27))  #<class 'float'>
+print(type(1027))  #<class 'int'>
+print(type('1027'))  #<class 'str'>
+print(10/27)  #java: 0, python:0.37037037037037035
+print(27/10)  #java: 2, python:2.7
+print(27//10)   #python: 나눈 몫: 2
+print(27%10)   #python: 나머지: 7
+print(type(10/27))  #<class 'float'>
+print(type(10//27))  #<class 'int'>
+print(10/5) #2.0    <class 'float'>
+
