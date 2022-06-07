c9c4cf9db314bbedc73d969ad9c44dd6f5cb2899
SSep <ssep4u@gmail.com>
2022-06-07 09:41:22 +0900

    Quiz. 전화번호 -, /, 띄어쓰기 삭제

---
+++ 20220607_quiz.py
@@ -0,0 +1,39 @@
+#1. 휴대전화번호 입력하면 -, /, 띄어쓰기 없애고 숫자만 출력하자
+phone_number = '010-7240-4658'
+for n in phone_number:
+    if n == '-' or n == '/' or n == ' ':
+        continue
+    print(n, end='')
+print()
+phone_number = phone_number.replace('-', '')
+phone_number = phone_number.replace('/', '')
+phone_number = phone_number.replace(' ', '')
+print(phone_number)
+
+#>> 01072404658
+print('-'*20)
+
+#2. 학번 -> 학년, 반, 학과, 번호 출력하기
+student_number = '2108'
+#>> 2학년 1반 뉴미디어소프트웨어과 08번
+#-> 2학년 1반 뉴미디어소프트웨어과 8번
+#-> if문 안 쓰고
+print('-'*20)
+
+#3. N-sum
+number = 331
+#>>7
+number = 523523
+#>>20
+
+#4. 369게임(1~100)
+#>>1 2 짝 4 5 짝 ... 짝짝 100
+
+
+
+
+
+
+
+
+
