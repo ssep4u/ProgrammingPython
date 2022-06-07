53b8f80db14a10439d194410186e43a071782e13
SSep <ssep4u@gmail.com>
2022-06-07 09:55:30 +0900

    Quiz. 학번 -> 학년, 반, 학과, 번

--- 20220607_quiz.py
+++ 20220607_quiz.py
@@ -15,6 +15,19 @@ print('-'*20)
 
 #2. 학번 -> 학년, 반, 학과, 번호 출력하기
 student_number = '2108'
+# if student_number[1] == '1' or student_number[1] == '2':
+#     g = '뉴미디어소프트웨어과'
+# elif student_number[1] == '3' or student_number[1] == '4':
+#     g = '뉴미디어웹솔루션과'
+# elif student_number[1] == '5' or student_number[1] == '6':
+#     g = '뉴미디어디자인과'
+majors = ['뉴미디어소프트웨어과', '뉴미디어소프트웨어과',
+          '뉴미디어웹솔루션과', '뉴미디어웹솔루션과',
+          '뉴미디어디자인과', '뉴미디어디자인과']
+index = int(student_number[1])
+g = majors[index-1]
+print(f'{student_number[0]}학년 {student_number[1]}반')
+print(f'{g} {int(student_number[2:])}번')
 #>> 2학년 1반 뉴미디어소프트웨어과 08번
 #-> 2학년 1반 뉴미디어소프트웨어과 8번
 #-> if문 안 쓰고
