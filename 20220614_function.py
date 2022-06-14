37a524352479975057127356c906a49ed1e57151
SSep <ssep4u@gmail.com>
2022-06-14 10:16:39 +0900

    함수 연습. 파라미터, 위치 인자, 키워드 인자, 기본값있는 인자, 가변 인자

--- 20220614_function.py
+++ 20220614_function.py
@@ -67,4 +67,9 @@ def my_print6(name, *args):   #args 자료형은 tuple
 my_print5('안유진', 20, '아이브', '러브다이브')
 my_print6('안유진', 20, '아이브', '러브다이브')
 print('-' * 20)
-print('-' * 20)
+# def my_print7(name, age=20, group):  #기본값있는 파라미터 뒤에는 무조건 기본값있는 파라미터
+#     print(name + ': ' + str(age) + '살입니다.' + group + '소속입니다.')   #이름: 살입니다.
+#     print(name,': ', age, '살입니다.', group, '소속입니다.')   #이름:  20 살입니다.
+#     print(f'{name}: {age}살입니다. {group}소속입니다.')
+# my_print7('CL', 21)
+# print('-' * 20)
