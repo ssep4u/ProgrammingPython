b9606f50cdb2ea8656aad96ff5cdd178c9da2d5b
SSep <ssep4u@gmail.com>
2022-10-18 10:20:20 +0900

    start Drink

---
+++ amasvin/drink.py
@@ -0,0 +1,14 @@
+# Drink
+    # name
+    # price
+    # cup_size
+    # sugar
+    # ice
+# Drink <- Coffee
+# Drink <- Bubbletea
+    # pearl
+# 옵션들
+# 컵사이즈: 기본, 점보
+# 펄: 타피오카, 알로에, 곤약, 코코넛
+# 당도: 30%, 50%, 70%, 100%
+# 얼음량: 없음, 적게, 기본, 많게
