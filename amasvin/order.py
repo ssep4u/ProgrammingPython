d849798ddc028fabb67bf9494c03ddf012d33a24
SSep <ssep4u@gmail.com>
2022-11-08 10:21:13 +0900

    start Order

---
+++ amasvin/order.py
@@ -0,0 +1,20 @@
+class Order:
+    def __init__(self):
+        self.menu = []  #메뉴판
+        self.init_menu()
+        self.order_menu = []     #주문한 음료수 리스트
+    def __str__(self):
+        pass
+   
+    def init_menu(self):    #메뉴판 초기화
+        pass
+    def order(self):
+        #반복
+            self.show_menu()    #메뉴판 보여주자
+            #음료수 고르자(사용자 입력받자)
+            #새로운 음료수로 생성하고, 옵션들 주문받자
+            #주문한 음료수 리스트에 새로운 음료수 추가하자
+        #주문한 음료수 리스트 출력하자
+
+    def show_menu(self):
+        pass
