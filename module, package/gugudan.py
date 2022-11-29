5b9d7c83575c0537436cdcc38d9620d0deac734b
SSep <ssep4u@gmail.com>
2022-11-29 09:35:43 +0900

    import 모듈; 모듈.함수() or 모듈.변수

---
+++ module, package/gugudan.py
@@ -0,0 +1,6 @@
+def gugudan(dan):
+    for i in range(1, 9+1):
+        print(f'{dan} x {i} = {dan * i}')
+
+if __name__ == '__main__':
+    gugudan(3)
--- module, package/insa2.py
+++ module, package/insa2.py
@@ -1,4 +1,6 @@
 import hello2
 import hello_func as h
+import gugudan as g
 
 h.hello()
+g.gugudan(5)
